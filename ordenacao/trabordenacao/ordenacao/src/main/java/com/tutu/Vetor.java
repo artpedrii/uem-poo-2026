b6c9fed5f351f268c5c33d56c55ed5f17310648d
Arthur Pedrinho de Paula <155795307+artpedrii@users.noreply.github.com>
2026-05-07 15:48:13 -0300

    Add files via upload

---
+++ ordenacao/trabordenacao/ordenacao/src/main/java/com/tutu/Vetor.java
@@ -0,0 +1,33 @@
+package com.tutu;
+
+public class Vetor {
+    private int[] elementos;
+
+    //construtor
+    public Vetor(int[] elementos) {
+        this.elementos = elementos;
+    }
+    
+    //getter
+    public int[] getElementos() {
+        return elementos;
+    }
+
+    //metodo
+    public void ordena(){
+        for (int j = 0; j < (elementos.length - 1); j++){
+            for (int i = 0; i < (elementos.length - 1); i++){
+                if (elementos[i] >= elementos[i + 1]){
+                    int pivo = elementos[i];
+                    elementos[i] = elementos[i + 1];
+                    elementos[i + 1] = pivo;
+                }
+            }
+        }
+    }
+    public void exibirVetor(){
+        for (int i = 0; i < elementos.length; i++){
+            System.out.printf("%d ", elementos[i]);
+        }
+    }
+}
