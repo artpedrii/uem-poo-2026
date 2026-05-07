459f093ae490f4cecda7a251b7fcd9efcc7d6d50
Arthur Pedrinho de Paula <155795307+artpedrii@users.noreply.github.com>
2026-05-07 15:48:26 -0300

    Delete ordenacao directory

--- ordenacao/trabordenacao/ordenacao/src/main/java/com/tutu/Main.java
+++
@@ -1,9 +0,0 @@
-package com.tutu;
-
-public class Main {
-    public static void main(String[] args) {
-        Vetor v1 = new Vetor(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
-        v1.ordena();
-        v1.exibirVetor();
-    }
-}
