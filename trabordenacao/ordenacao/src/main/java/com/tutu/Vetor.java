046b7b183421f45a2c9a2e9c93ed30e2ec9e9306
Arthur Pedrinho de Paula <155795307+artpedrii@users.noreply.github.com>
2026-05-10 20:14:05 -0300

    aleracaofinal

--- trabordenacao/ordenacao/src/main/java/com/tutu/Vetor.java
+++ trabordenacao/ordenacao/src/main/java/com/tutu/Vetor.java
@@ -1,33 +1,35 @@
 package com.tutu;
 
+import java.util.List;
+
 public class Vetor {
-    private List elementos;
+    private List<Integer> elementos;
 
     //construtor
-    public Vetor(int[] elementos) {
+    public Vetor(List<Integer> elementos) {
         this.elementos = elementos;
     }
     
     //getter
-    public int[] getElementos() {
+    public List<Integer> getElementos() {
         return elementos;
     }
 
     //metodo
     public void ordena(){
-        for (int j = 0; j < (elementos.length - 1); j++){
-            for (int i = 0; i < (elementos.length - 1); i++){
-                if (elementos[i] >= elementos[i + 1]){
-                    int pivo = elementos[i];
-                    elementos[i] = elementos[i + 1];
-                    elementos[i + 1] = pivo;
+        for (int j = 0; j < (elementos.size() - 1); j++){
+            for (int i = 0; i < (elementos.size() - 1); i++){
+                if (elementos.get(i) >= elementos.get(i + 1)){
+                    int pivo = elementos.get(i);
+                    elementos.set(i, elementos.get(i + 1)) ;
+                    elementos.set(i + 1, pivo);
                 }
             }
         }
     }
     public void exibirVetor(){
-        for (int i = 0; i < elementos.length; i++){
-            System.out.printf("%d ", elementos[i]);
+        for (int i = 0; i < elementos.size(); i++){
+            System.out.printf("%d ", elementos.get(i));
         }
     }
 }
