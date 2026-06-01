2da44633c56b8768aa171e2e37cda36d23c6e94f
Arthur Pedrinho de Paula <arthurppaula265@gmail.com>
2026-06-01 09:03:46 -0300

    commit

---
+++ praticasaula/0206/tutu/src/test/java/com/tutu/tutu/TutuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutu.tutu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
