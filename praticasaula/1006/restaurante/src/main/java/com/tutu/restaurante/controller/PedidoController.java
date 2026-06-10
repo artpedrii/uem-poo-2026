c35c580dd2bcc443471c3535164dbdf98e6eaeb9
Arthur Pedrinho de Paula <arthurppaula265@gmail.com>
2026-06-10 09:06:52 -0300

    commit

--- praticasaula/1006/restaurante/src/main/java/com/tutu/restaurante/controller/PedidoController.java
+++ praticasaula/1006/restaurante/src/main/java/com/tutu/restaurante/controller/PedidoController.java
@@ -8,6 +8,7 @@ import com.tutu.restaurante.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class PedidoController {
     private PedidoService pedidoService;
     
     @PostMapping
-    public Pedido criarPedido(Pedido pedido){
+    public Pedido criarPedido(@RequestBody Pedido pedido){
         return pedidoService.criarPedido(pedido);
     }
 
