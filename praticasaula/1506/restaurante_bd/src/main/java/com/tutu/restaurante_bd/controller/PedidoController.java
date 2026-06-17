4a0226cea0dc3ed1ecaf1017932ee134a2c76bac
Guest <guest-au554z@lin>
2026-06-17 08:46:31 -0300

    aa

--- praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/controller/PedidoController.java
+++ praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/controller/PedidoController.java
@@ -1,5 +1,23 @@
 package com.tutu.restaurante_bd.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tutu.restaurante_bd.model.Pedido;
+import com.tutu.restaurante_bd.service.PedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
-    
+    @Autowired
+    private PedidoService pedidoService;
+
+    @GetMapping
+    public List<Pedido> lerPedidos(){
+        return pedidoService.lerPedidos();
+    }
 }
--- praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/service/PedidoService.java
+++ praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/service/PedidoService.java
@@ -1,9 +1,26 @@
 package com.tutu.restaurante_bd.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tutu.restaurante_bd.model.Pedido;
 import com.tutu.restaurante_bd.repository.PedidoRepository;
 
+@Service
 public class PedidoService {
+
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    public List<Pedido> lerPedidos(){
+        return pedidoRepository.findAll();
+    }
+
+    //public Pedido inserirPedido(){
+
+    //}
+
+    
 }
