620d892ba837f9f2eef02d90b26491c7873a4193
Guest <guest-au554z@lin>
2026-06-17 09:13:46 -0300

    vks

--- praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/controller/PedidoController.java
+++ praticasaula/1506/restaurante_bd/src/main/java/com/tutu/restaurante_bd/controller/PedidoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class PedidoController {
         return pedidoService.lerPedidos();
     }
 
+    @PostMapping
     public Pedido inserirPedido(@RequestBody Pedido pedido){
         return pedidoService.inserirPedido(pedido);
     }
